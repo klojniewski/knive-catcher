a802db1b22dc9a7e9fe22aa9b780b04d7b475db6
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-09-16 22:07:25 +0200

    async / await rewrite

--- index.js
+++ index.js
@@ -4,31 +4,26 @@ const TickerModel = require('./models/Ticker')
 const Validator = require('./lib/validator')
 const Calculator = require('./lib/calculator')
 
-class Backtester {
-  constructor () {
-    Mongoose.connect(Env.DB_URL, { useMongoClient: true })
-    Mongoose.Promise = global.Promise
-  }
-  init () {
-    console.info('Backtester Init')
+backtest()
 
-    var stream = TickerModel.find().cursor()
-
-    stream.on('data', function (ticker) {
-      if (ticker.currencyPair != 'xxx') {
-        if (
-            Validator.isLowest(ticker.last, ticker.dayLow)
-          ) {
-          console.log('lowest', Calculator.dailyPercentChangeFromHigh(ticker.last, ticker.dayHigh))
-        }
-      }
-    }).on('error', function (err) {
-  // handle the error
-    }).on('close', function () {
-  // the stream is closed
-    })
-  }
+async function wait (ms) {
+  await new Promise(resolve => setTimeout(() => resolve(), ms))
+  console.log('waited', ms)
+  return ms
 }
 
-const backtest = new Backtester()
-backtest.init()
+async function backtest () {
+  Mongoose.connect(Env.DB_URL, { useMongoClient: true })
+  Mongoose.Promise = global.Promise
+
+  // Don't `await`, instead get a cursor
+  const cursor = TickerModel.find().cursor()
+  // Use `next()` and `await` to exhaust the cursor
+  for (let ticker = await cursor.next(); ticker != null; ticker = await cursor.next()) {
+    if (ticker.currencyPair !== 'xxx') {
+      if (Validator.isLowest(ticker.last, ticker.dayLow)) {
+        console.log('lowest', Calculator.dailyPercentChangeFromHigh(ticker.last, ticker.dayHigh))
+      }
+    }
+  }
+}
